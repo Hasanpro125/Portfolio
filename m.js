f50a4061f0101144fc0c7edc49f8411415476d4b
Hasan Olomboyev <olimboyevhas209@gmail.com>
2023-03-01 22:16:49 +0500

    add files

---
+++ m.js
@@ -0,0 +1,48 @@
+let = menuIcons = document.querySelectorAll('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcons.onclick = () => {
+    menuIcons.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offsetTop = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100)
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Fronted Developer', 'Designer', 'Youtuber'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
